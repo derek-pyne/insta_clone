86b81638443d41f3ce5ef02ca2acf5029c99b154
Derek Pyne <derek.pyne@gmail.com>
2019-12-11 22:22:27 -0500

    Styling instagram posts on homepage

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
+from app.models import InfluencerPost
 from . import main
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    influencer_posts = InfluencerPost.query.all()
+    return render_template('index.html', influencer_posts=influencer_posts)
