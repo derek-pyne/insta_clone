6a98a7b4366cfd40d2775c8f07a2bd86038f08c6
Derek Pyne <derek.pyne@shopify.com>
2019-12-23 17:36:49 -0500

    Post resource page with form for editing

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms_components import read_only
 
 
 class EditPostForm(FlaskForm):
-    caption = StringField('Enter your caption', validators=[DataRequired()])
+    influencer_caption = StringField('Influencer caption:', validators=[DataRequired()])
+    caption = StringField('Caption:', validators=[DataRequired()])
+    alt_text = StringField('Alt Text:', validators=[DataRequired()])
+    influencer = StringField('Influencer:', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(EditPostForm, self).__init__(*args, **kwargs)
+        read_only(self.influencer)
+        read_only(self.influencer_caption)
--- app/main/views.py
+++ app/main/views.py
@@ -3,23 +3,25 @@ from flask import render_template, flash, redirect, url_for
 from app.main.forms import EditPostForm
 from app.models import Post
 from . import main
+from .. import db
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET'])
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 
-@main.route('/edit/<string:id>', methods=['GET', 'POST'])
-def edit(id):
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+def post(id):
     post = Post.query.get_or_404(id)
-    form = EditPostForm()
+    form = EditPostForm(obj=post)
     if form.validate_on_submit():
-        # post.body = form.body.data
-        # db.session.add(post)
-        # db.session.commit()
+        form.populate_obj(post)
+        db.session.add(post)
+        db.session.commit()
         flash('The post has been updated.')
         return redirect(url_for('.post', id=post.id))
-    form.caption.data = post.caption
-    return render_template('edit_post.html', form=form)
+    if not post.caption:
+        form.caption.data = post.influencer_caption
+    return render_template('post.html', post=post, form=form)
