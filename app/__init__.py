972a78f5665273f654e790f6868e0a1999168384
Derek Pyne <derek.pyne@shopify.com>
2019-12-25 09:56:28 -0500

    User object and initial auth flows

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from config import config
@@ -8,6 +9,8 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 ma = Marshmallow()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,11 +20,15 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     ma.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 
 from app.main.forms import EditPostForm
-from app.models import Post
+from app.models import Post, User
 from . import main
 from .. import db
 
@@ -10,6 +10,12 @@ from .. import db
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
+
+
+@main.route('/user/<int:id>', methods=['GET'])
+def user(id):
+    user = User.query.get_or_404(id)
+    return render_template('index.html', user=user)
 
 
 @main.route('/post/<int:id>', methods=['GET', 'POST'])
